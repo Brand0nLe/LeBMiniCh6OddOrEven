db56466b0c1f0f273b248b6a7afd718a72d6c792
Brand0nLe <93brandonle@gmail.com>
2022-10-20 19:28:50 -0700

    Final fix and clean up of code

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@
 //Project will take user's input and print into the terminal whether
 //the number is even or odd. It will also have an error for when user
 //input is not a number. Will also have play again function.
+//Peer Review: Arely Martinez - The program did what it was intended. The number i input was declared as odd or even.
+//There was also data confirmation for both if it was a vaild entry when i first entered in a number.
+//I like how after the second time i inputed a invaild option it returned me to the begining of the program. However there was a small issue with data confirmation
+//at the end of Brandon's work. So when i enterered in something that wasnt 'yes' or 'no' the program would make me replay the game.
+//I told Brandon about the issue and he said he would correct it, which is great that he is willing to change his errors.
 
 
 string userInput;
@@ -17,6 +22,7 @@ bool playAgain = true;
 
 while (playAgain == true)
 {
+    Console.Clear();
     Console.WriteLine("Hello! If you give me a number I'll tell you if it's odd or even!");
     Console.WriteLine("Enter your favorite number:");
     userInput = Console.ReadLine();
@@ -42,6 +48,12 @@ else
 }
 
 userInputEnd = Console.ReadLine().ToUpper();
+
+while(userInputEnd != "YES" && userInputEnd != "NO")
+{
+    Console.WriteLine("ERROR! Please enter YES or NO:");
+    userInputEnd = Console.ReadLine().ToUpper();
+}
 
 if(userInputEnd == "YES")
 {
